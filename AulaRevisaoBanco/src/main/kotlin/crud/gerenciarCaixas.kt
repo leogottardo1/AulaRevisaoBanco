b1117fb82f0ae326ac2bfc9d7ae017c8ae78138d
leogottardo1 <168039941+leogottardo1@users.noreply.github.com>
2025-08-27 20:25:44 -0300

    27082025

---
+++ AulaRevisaoBanco/src/main/kotlin/crud/gerenciarCaixas.kt
@@ -0,0 +1,43 @@
+package crud
+
+import enumeradores.Material
+
+fun cadastarCaixa(){
+    /*
+    class CaixaDAgua (
+    val material: Material,
+    val capacidade: Double?,
+    val altura: Double,
+    val largura: Double,
+    val profundida: Double,
+    val blablablabla: String,
+     PLASTICO, PVC, METAL, ARGAMASSA
+    */
+    println("Escolha o material de qual a caixa é composta:")
+    println("1 - Plastico")
+    println("2 - PVC")
+    println("3 - Metal")
+    println("4 - Argamassa")
+    val opcao = readln().toInt()
+    var material : Material
+    when(opcao) {
+        1-> material = Material.PLASTICO
+        2-> material = Material.PVC
+        3-> material = Material.METAL
+        4-> material = Material.ARGAMASSA
+        else -> material = Material.PLASTICO
+    }
+    println("Capacidade da Caixa em Litros:")
+    val litros = readln().toDouble()
+
+    println("Largura da caixa:")
+}
+fun editarCaixa(){
+
+}
+fun listarCaixas(){
+
+}
+fun excluirCaixa(){
+
+}
--- AulaRevisaoBanco/src/main/kotlin/ui/Menu.kt
+++ AulaRevisaoBanco/src/main/kotlin/ui/Menu.kt
@@ -1,8 +1,21 @@
 package ui
 
 fun menu(){
-    println("1 - Cadastrar Caixa D Agua")
-    println("2 - Editar Caixa de Agua")
-    println("3 - Listar Caixa de Agua")
-    println("4 - Excluir Caixa de Agua")
-}
+    do {
+        println("1 - Cadastrar Caixa D Agua")
+        println("2 - Editar Caixa de Agua")
+        println("3 - Listar Caixa de Agua")
+        println("4 - Excluir Caixa de Agua")
+        println("0 - Sair")
+
+        val opcao = readln().toInt()
+        when (opcao) {
+            0 -> println("Adeus amigo")
+            1 -> println("Cadastrando caixa....")
+            2 -> println("Editando Caixa.......")
+            3 -> println("Listar caixas......")
+            4 -> println("Cadastrando caixa de agua...")
+            else -> println("Opcao invalida...")
+        }
+        }while (opcao != 0 )
+    }
